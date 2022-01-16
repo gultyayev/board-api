e2f7fa9d0a1ad87d66002d3dc0630cf7dcf244c6
Сергей <gultyayev.sergey@gmail.com>
2022-01-16 20:15:56 +0200

    Store users locally

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+
+const path = 'users.json';
 
 @Injectable()
 export class UsersService {
-  private readonly users: Map<string, string> = new Map();
+  private users: Record<string, string> = {};
 
   constructor() {
-    this.addUser('test', 'test');
+    const users = fs.readFileSync(path, {
+      encoding: 'utf-8',
+      flag: 'a+',
+    });
+    const obj: Record<string, string> = JSON.parse(users) || {};
+
+    if (Object.keys(obj).length) {
+      this.users = obj;
+    } else {
+      this.addUser('test', 'test');
+    }
+
+    console.log({ users: this.users });
   }
 
   validateUser(username: string, password: string): boolean {
-    const pass = this.users.get(username);
+    const pass = this.users[username];
     return (pass && pass === password) || null;
   }
 
   hasUser(username: string): boolean {
-    return this.users.has(username);
+    return username in this.users;
   }
 
   addUser(username: string, password: string): void {
-    this.users.set(username, password);
+    this.users[username] = password;
+    fs.writeFileSync(path, JSON.stringify(this.users), {
+      encoding: 'utf-8',
+    });
   }
 }
